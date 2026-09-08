563bc955a8ea32c3fc162f9d439d0f3e50dac97c
ntn9528146 <ntn9528146@gmail.com>
2026-09-08 09:06:33 +0530

    Fix secondary index to expose all 17 subjects to Syllabus selector

---
+++ src/config/syllabus/secondary/index.js
@@ -0,0 +1,8 @@
+import { secondarySyllabus } from './syllabus.js';
+
+export const secondaryData = {
+  classes: secondarySyllabus.classes,
+  subjects: secondarySyllabus.subjects
+};
+
+export default secondaryData;
