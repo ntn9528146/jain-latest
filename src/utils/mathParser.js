5cca5ad6e57e1f6f9a3bfbd7e634b46876c7dea1
ntn9528146 <ntn9528146@gmail.com>
2026-09-09 16:02:53 +0530

    Update mathParser to correctly format all subscript and superscript patterns

--- src/utils/mathParser.js
+++ src/utils/mathParser.js
@@ -6,18 +6,18 @@ export function formatMathText(text) {
   // 1. Fractions: \frac{num}{den} -> stacked fraction format
   processed = processed.replace(/\\frac\{([^}]+)\}\{([^}]+)\}/g, '<span style="display:inline-flex; flex-direction:column; vertical-align:middle; text-align:center; margin:0 3px; font-size:0.95em;"><span style="border-bottom:1px solid currentColor; padding:0 2px;">$1</span><span style="padding:0 2px;">$2</span></span>');
 
-  // 2. Superscripts (Powers like x^2 or x^{2})
+  // 2. Superscripts (Powers like x^2, x^{2})
   processed = processed.replace(/\^\{([^}]+)\}/g, '<sup>$1</sup>');
   processed = processed.replace(/\^([0-9a-zA-Z+\-]+)/g, '<sup>$1</sup>');
 
-  // 3. Subscripts (Like H_2O or a_1)
+  // 3. Subscripts (Like CH_3, CH_{3}, f_1, f_{1})
   processed = processed.replace(/_\{([^}]+)\}/g, '<sub>$1</sub>');
-  processed = processed.replace(/_([0-9a-zA-Z+\-]+)/g, '<sub>$1</sub>');
+  processed = processed.replace(/_([0-9a-zA-Z])/g, '<sub>$1</sub>');
 
   // 4. Square roots (\sqrt{x})
   processed = processed.replace(/\\sqrt\{([^}]+)\}/g, '√(<i>$1</i>)');
 
-  // 5. Common mathematical symbols & cleanup
+  // 5. Common mathematical/chemical symbols & cleanup
   processed = processed
     .replace(/\\pi/g, 'π')
     .replace(/\\theta/g, 'θ')
